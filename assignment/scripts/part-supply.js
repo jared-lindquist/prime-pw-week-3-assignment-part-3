715cb32ff1d53cc1dfb0def6debb4bf81b2ff56d
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-10-04 16:40:35 -0500

    partial on #7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -43,6 +43,12 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+supplyChanges = [3, 5, -6, 0, 7, 25]
+for (let x of supplyChanges){
+    let element = supplyChanges[x];
+	 element > 0 ? console.log('Added ' + element + ' parts.') : element === 0 ? console.log('No Change.') : element < 0 ? console.log('Removed ' + element + ' parts.'): null
+}
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
