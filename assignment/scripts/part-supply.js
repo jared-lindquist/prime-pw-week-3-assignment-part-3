33df63577dd28e1c2bfe9cbdfeb5c0537e2c4095
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-10-04 12:32:25 -0500

    first part of #6maybe done

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -9,7 +9,7 @@ console.log(partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-let supplyChanges = ['3', '5', '-6', '0', '7', '11'];
+let supplyChanges = [3, 5, -6, 0, 7, 11];
 console.log(supplyChanges);
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is: ');
@@ -22,7 +22,7 @@ console.log(supplyChanges[5]);
 supplyChanges.pop();
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-supplyChanges.push('25');
+supplyChanges.push(25);
 console.log(supplyChanges);
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -30,8 +30,15 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 
-
 console.log('6. Showing supplyChanges...');
+const positiveArr = [];
+for(let x = 0; x < supplyChanges.length; x++){
+    if (supplyChanges[x] > 0){
+        positiveArr.push(supplyChanges[x]);
+    }
+}
+
+console.log('added ' + '' + positiveArr + '' + ' parts.');
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
